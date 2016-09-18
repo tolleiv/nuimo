704d750c16c1f2ebed228937e85c5d14589eb913
tolleiv <github@tolleiv.de>
2016-09-18 23:25:16 +0200

    Adjust error handling and logging and try reconnects if timeouts were found

--- nuimo.go
+++ nuimo.go
@@ -1,23 +1,25 @@
 package nuimo
 
 import (
-	"log"
+	"encoding/binary"
 	"fmt"
 	"strings"
+	"time"
+
 	"github.com/currantlabs/ble"
 	"github.com/currantlabs/ble/examples/lib/gatt"
 	"github.com/currantlabs/ble/linux/hci"
 	"github.com/currantlabs/ble/linux/hci/cmd"
-	"encoding/binary"
+	"github.com/mgutz/logxi/v1"
 )
 
-const SERVICE_BATTERY_STATUS = "0000180F00001000800000805F9B34FB"
-const SERVICE_DEVICE_INFO = "0000180A00001000800000805F9B34FB"
+const SERVICE_BATTERY_STATUS = "180F"
+const SERVICE_DEVICE_INFO = "180A"
 const SERVICE_LED_MATRIX = "F29B1523CB1940F3BE5C7241ECB82FD1"
 const SERVICE_USER_INPUT = "F29B1525CB1940F3BE5C7241ECB82FD2"
 
-const CHAR_BATTERY_LEVEL = "00002A1900001000800000805F9B34FB"
-const CHAR_DEVICE_INFO = "00002A2900001000800000805F9B34FB"
+const CHAR_BATTERY_LEVEL = "2A19"
+const CHAR_DEVICE_INFO = "2A29"
 const CHAR_LED_MATRIX = "F29B1524CB1940F3BE5C7241ECB82FD1"
 const CHAR_INPUT_FLY = "F29B1526CB1940F3BE5C7241ECB82FD2"
 const CHAR_INPUT_SWIPE = "F29B1527CB1940F3BE5C7241ECB82FD2"
@@ -35,10 +37,14 @@ const DIR_UPDOWN = 4
 const CLICK_DOWN = 1
 const CLICK_UP = 0
 
+var logger = log.New("nuimo")
+
 type Nuimo struct {
 	client ble.Client
 	events chan Event
 	led    *ble.Characteristic
+	bttry  *ble.Characteristic
+	nfo    *ble.Characteristic
 }
 
 type Event struct {
@@ -47,7 +53,25 @@ type Event struct {
 	Raw   []byte
 }
 
-func Connect() (*Nuimo, error) {
+func Connect(params ...int) (*Nuimo, error) {
+
+	ch := make(chan Event, 100)
+	n := &Nuimo{events: ch}
+	err := n.reconnect()
+
+	if err != nil {
+		logger.Fatal("%s", err)
+	}
+
+	if len(params) == 1 && params[0] > 0 {
+		go n.keepConnected(params[0])
+	}
+
+	return n, err
+}
+
+func discoverDevice() (ble.Client, error) {
+	logger.Info("Discover")
 	filter := func(a ble.Advertisement) bool {
 		return strings.ToUpper(a.LocalName()) == "NUIMO"
 	}
@@ -57,32 +81,60 @@ func Connect() (*Nuimo, error) {
 	if h, ok := d.(*hci.HCI); ok {
 		if err := h.Option(hci.OptConnParams(
 			cmd.LECreateConnection{
-				LEScanInterval:        0x0004, // 0x0004 - 0x4000; N * 0.625 msec
-				LEScanWindow:          0x0004, // 0x0004 - 0x4000; N * 0.625 msec
-				InitiatorFilterPolicy: 0x00, // White list is not used
-				PeerAddressType:       0x00, // Public Device Address
+				LEScanInterval:        0x0004,    // 0x0004 - 0x4000; N * 0.625 msec
+				LEScanWindow:          0x0004,    // 0x0004 - 0x4000; N * 0.625 msec
+				InitiatorFilterPolicy: 0x00,      // White list is not used
+				PeerAddressType:       0x00,      // Public Device Address
 				PeerAddress:           [6]byte{}, //
-				OwnAddressType:        0x00, // Public Device Address
-				ConnIntervalMin:       0x0006, // 0x0006 - 0x0C80; N * 1.25 msec
-				ConnIntervalMax:       0x0006, // 0x0006 - 0x0C80; N * 1.25 msec
-				ConnLatency:           0x0000, // 0x0000 - 0x01F3; N * 1.25 msec
-				SupervisionTimeout:    0x0048, // 0x000A - 0x0C80; N * 10 msec
-				MinimumCELength:       0x0000, // 0x0000 - 0xFFFF; N * 0.625 msec
-				MaximumCELength:       0x0000, // 0x0000 - 0xFFFF; N * 0.625 msec
+				OwnAddressType:        0x00,      // Public Device Address
+				ConnIntervalMin:       0x0006,    // 0x0006 - 0x0C80; N * 1.25 msec
+				ConnIntervalMax:       0x0006,    // 0x0006 - 0x0C80; N * 1.25 msec
+				ConnLatency:           0x0000,    // 0x0000 - 0x01F3; N * 1.25 msec
+				SupervisionTimeout:    0x0048,    // 0x000A - 0x0C80; N * 10 msec
+				MinimumCELength:       0x0000,    // 0x0000 - 0xFFFF; N * 0.625 msec
+				MaximumCELength:       0x0000,    // 0x0000 - 0xFFFF; N * 0.625 msec
 			})); err != nil {
-			log.Fatalf("can't set advertising param: %s", err)
+			logger.Fatal("can't set advertising param: %s", err)
 		}
 	}
+	return gatt.Discover(gatt.FilterFunc(filter))
+}
 
-	client, err := gatt.Discover(gatt.FilterFunc(filter))
-	if err != nil {
-		return nil, err
+func (n *Nuimo) reconnect() error {
+	logger.Info("Reconnect")
+	if n.client != nil {
+		n.client.ClearSubscriptions()
+		n.client.CancelConnection()
 	}
+	client, err := discoverDevice()
+	if err != nil {
+		return err
+	}
+	n.client = client
+	return n.DiscoverServices()
+}
 
-	ch := make(chan Event)
-	n := &Nuimo{client: client, events: ch}
-	err = n.DiscoverServices()
-	return n, err
+func (n *Nuimo) keepConnected(refresh int) {
+
+	for {
+		c := make(chan uint8, 1)
+		go func() {
+			logger.Info("Reading batterie")
+			data, err := n.client.ReadCharacteristic(n.bttry)
+			if err != nil {
+				logger.Fatal("Error", err)
+			}
+			c <- uint8(data[0])
+		}()
+		select {
+		case data := <-c:
+			logger.Info("Batterie level", data)
+		case <-time.After(30 * time.Second):
+			n.reconnect()
+		}
+		close(c)
+		time.Sleep(10 * time.Second)
+	}
 
 }
 
@@ -96,7 +148,7 @@ func (n *Nuimo) Display(matrix []byte, brightness uint8, timeout uint8) {
 
 	for c, dots := range matrix {
 		if c > 10 {
-			break;
+			break
 		}
 		displayMatrix[c] = dots
 	}
@@ -126,6 +178,7 @@ func DisplayMatrix(dots ...byte) []byte {
 	return bytes
 }
 
+//  TODO: make sure we only subscribe to the services we need
 func (n *Nuimo) DiscoverServices() error {
 	p, err := n.client.DiscoverProfile(true)
 	if err != nil {
@@ -134,15 +187,33 @@ func (n *Nuimo) DiscoverServices() error {
 
 	for _, s := range p.Services {
 
-		if s.UUID.Equal(ble.MustParse(SERVICE_BATTERY_STATUS)) {
+		switch {
+		case s.UUID.Equal(ble.MustParse(SERVICE_DEVICE_INFO)):
 			for _, c := range s.Characteristics {
-				if (c.UUID.Equal(ble.MustParse(CHAR_BATTERY_LEVEL))) {
-					n.client.Subscribe(c, false, n.battery)
+				switch {
+				case c.UUID.Equal(ble.MustParse(CHAR_DEVICE_INFO)):
+					n.nfo = c
+					logger.Info("Info subscribed")
+					n.client.Subscribe(c, false, n.info)
+				default:
+					logger.Warn("Unknown device char", "uuid", c.UUID.String())
+					n.client.Subscribe(c, false, n.unknown)
 				}
 			}
-		}
+		case s.UUID.Equal(ble.MustParse(SERVICE_BATTERY_STATUS)):
+			for _, c := range s.Characteristics {
+				switch {
+				case c.UUID.Equal(ble.MustParse(CHAR_BATTERY_LEVEL)):
+					logger.Info("Battery subscribed")
+					n.bttry = c
+					n.client.Subscribe(c, false, n.battery)
 
-		if s.UUID.Equal(ble.MustParse(SERVICE_USER_INPUT)) {
+				default:
+					logger.Warn("Unknown battery char", "uuid", c.UUID.String())
+					n.client.Subscribe(c, false, n.unknown)
+				}
+			}
+		case s.UUID.Equal(ble.MustParse(SERVICE_USER_INPUT)):
 			for _, c := range s.Characteristics {
 
 				switch {
@@ -156,24 +227,25 @@ func (n *Nuimo) DiscoverServices() error {
 				case c.UUID.Equal(ble.MustParse(CHAR_INPUT_FLY)):
 					n.client.Subscribe(c, false, n.fly)
 				default:
-					log.Printf("Unknown characteristik %s", c.UUID.String())
+					logger.Warn("Unknown input characteristik", "uuid", c.UUID.String())
 					n.client.Subscribe(c, false, n.unknown)
 
 				}
-
 			}
-		}
-
-		if s.UUID.Equal(ble.MustParse(SERVICE_LED_MATRIX)) {
+		case s.UUID.Equal(ble.MustParse(SERVICE_LED_MATRIX)):
 			for _, c := range s.Characteristics {
+				logger.Info("Led found")
 				n.led = c
 			}
+		default:
+			logger.Warn("Unknown service %s", "uuid", s.UUID.String())
 		}
 	}
 	return nil
 }
 
 func (n *Nuimo) Disconnect() error {
+	logger.Warn("Nuimo connection closed")
 	close(n.events)
 	return n.client.CancelConnection()
 }
@@ -181,7 +253,10 @@ func (n *Nuimo) Disconnect() error {
 func (n *Nuimo) battery(req []byte) {
 	uval, _ := binary.Uvarint(req)
 	level := int64(uval)
-	n.events <- Event{Key:"battery", Raw: req, Value: level}
+	n.send(Event{Key: "battery", Raw: req, Value: level})
+}
+func (n *Nuimo) info(req []byte) {
+	logger.Info("Info: " + string(req))
 }
 
 func (n *Nuimo) click(req []byte) {
@@ -189,31 +264,31 @@ func (n *Nuimo) click(req []byte) {
 	dir := int64(uval)
 	switch dir {
 	case CLICK_DOWN:
-		n.events <- Event{Key:"press", Raw: req}
+		n.send(Event{Key: "press", Raw: req})
 	case CLICK_UP:
-		n.events <- Event{Key:"release", Raw: req}
+		n.send(Event{Key: "release", Raw: req})
 	}
 }
 
 func (n *Nuimo) rotate(req []byte) {
 	uval := binary.LittleEndian.Uint16(req)
 	val := int64(int16(uval))
-	n.events <- Event{Key:"rotate", Raw: req, Value: val}
+	n.send(Event{Key: "rotate", Raw: req, Value: val})
 }
 func (n *Nuimo) swipe(req []byte) {
 	uval, _ := binary.Uvarint(req)
 	dir := int64(uval)
-	n.events <- Event{Key:"swipe", Raw: req, Value: dir}
+	n.send(Event{Key: "swipe", Raw: req, Value: dir})
 
 	switch dir {
 	case DIR_LEFT:
-		n.events <- Event{Key:"swipe_left", Raw: req}
+		n.send(Event{Key: "swipe_left", Raw: req})
 	case DIR_RIGHT:
-		n.events <- Event{Key:"swipe_right", Raw: req}
+		n.send(Event{Key: "swipe_right", Raw: req})
 	case DIR_UP:
-		n.events <- Event{Key:"swipe_up", Raw: req}
+		n.send(Event{Key: "swipe_up", Raw: req})
 	case DIR_DOWN:
-		n.events <- Event{Key:"swipe_down", Raw: req}
+		n.send(Event{Key: "swipe_down", Raw: req})
 	}
 }
 func (n *Nuimo) fly(req []byte) {
@@ -224,17 +299,22 @@ func (n *Nuimo) fly(req []byte) {
 
 	switch dir {
 	case DIR_LEFT:
-		n.events <- Event{Key:"fly_left", Raw: req, Value: distance}
+		n.send(Event{Key: "fly_left", Raw: req, Value: distance})
 	case DIR_RIGHT:
-		n.events <- Event{Key:"fly_right", Raw: req, Value: distance}
+		n.send(Event{Key: "fly_right", Raw: req, Value: distance})
 	case DIR_BACKWARDS:
-		n.events <- Event{Key:"fly_backwards", Raw: req, Value: distance}
+		n.send(Event{Key: "fly_backwards", Raw: req, Value: distance})
 	case DIR_TOWARDS:
-		n.events <- Event{Key:"fly_towards", Raw: req, Value: distance}
+		n.send(Event{Key: "fly_towards", Raw: req, Value: distance})
 	case DIR_UPDOWN:
-		n.events <- Event{Key:"fly_updown", Raw: req, Value: distance}
+		n.send(Event{Key: "fly_updown", Raw: req, Value: distance})
 	}
 }
 func (n *Nuimo) unknown(req []byte) {
-	n.events <- Event{Key:"unknown", Raw: req}
+	n.send(Event{Key: "unknown", Raw: req})
+}
+
+// make sure missing event sinks don't block the client
+func (n *Nuimo) send(e Event) {
+	go func() { n.events <- e }()
 }
